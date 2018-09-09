df685e0b9ee3ffb4899be366f563e7e0a6d8a2dd
Zane Wang <363177773@qq.com>
2018-09-08 17:58:50 -0700

    interview

---
+++ src/interview/facebook/IncreasingContainer.java
@@ -0,0 +1,25 @@
+package interview.facebook;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class IncreasingContainer {
+
+//    public List<Integer> rebuild(Container c) {
+//        List<Integer> result = new LinkedList<>();
+//        Stack<Integer> stack = new Stack<>();
+//        while (!c.isEmpty()) {
+//            int i = c.get();
+//            while (!stack.isEmpty() && i > stack.peek()) {
+//                result.add(stack.pop());
+//            }
+//            stack.push(i);
+//        }
+//        while (!stack.isEmpty()) {
+//            result.add(stack.pop());
+//        }
+//        return result;
+//    }
+
+}
